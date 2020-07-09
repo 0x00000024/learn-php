2ca33a005b16628c47ea97bb70df223c6407e007
Ethan Yu <sohbmpethan@gmail.com>
2020-07-09 13:04:59 +0900

    01-first-program

---
+++ 01-first-program/dynamic_text.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php $title = 'Ethan'; ?>
+
+<h1><?php echo $title; ?></h1>
+
+</body>
+</html>
