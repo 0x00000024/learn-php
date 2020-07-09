b60fa5ca76bd006bc0b782ee1525f917cb6a0d6f
Ethan Yu <sohbmpethan@gmail.com>
2020-07-09 13:51:57 +0900

    practice-01

---
+++ 01-first-program/practice-01/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<div id="navigation">
+
+    <?php
+
+    include "functions.php";
+
+    Navigation();
+
+    ?>
+
+</div>
+
+<div id="content">
+
+</div>
+
+
+</body>
+</html>
---
+++ 01-first-program/practice-01/index.php
@@ -0,0 +1,14 @@
+<?php include "functions.php" ?>
+<?php include "includes/header.php" ?>
+
+    <section class="content">
+
+        <aside class="col-sm-4">
+
+            <?php Navigation(); ?>
+
+        </aside><!--SIDEBAR-->
+
+    </section>
+
+<?php include "includes/footer.php" ?>
