652007a5f69d821573eae26688183cafdb6dc6ba
Ethan Yu <sohbmpethan@gmail.com>
2020-07-09 13:26:29 +0900

    PHP Comments

---
+++ 01-first-program/comments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+// This is a single line comment
+
+/*
+ *
+ * multiple line comment
+ *
+ */
+
+echo "hello";
+
+?>
+
+
+</body>
+</html>
