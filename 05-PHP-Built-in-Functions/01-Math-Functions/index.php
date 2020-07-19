a23591fabba56a6181e2835d8cdca770eabc7bec
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 20:15:26 +0900

    01-Math-Functions

---
+++ 05-PHP-Built-in-Functions/01-Math-Functions/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+echo pow(2, 7) . "<br>";
+
+echo rand(1, 1000) . "<br>";
+
+echo sqrt(100) . "<br>";
+
+echo ceil(4.5) . "<br>";
+
+echo floor(4.5) . "<br>";
+
+echo round(4.3) . "<br>";
+
+?>
+
+</body>
+</html>
