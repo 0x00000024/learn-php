22031ea9010f327c9a4bd0b31fabe9125de7f799
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 20:17:59 +0900

    02-String-Functions

---
+++ 05-PHP-Built-in-Functions/02-String-Functions/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+$string = "Hello, my name is Ethan.";
+
+echo strlen($string) . "<br>";
+
+echo strtoupper($string) . "<br>";
+
+echo strtolower($string) . "<br>";
+
+?>
+
+</body>
+</html>
