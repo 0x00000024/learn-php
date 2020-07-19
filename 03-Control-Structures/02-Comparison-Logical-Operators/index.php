123512fd77bb242c878ad6209976fc795116cfe0
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 17:44:39 +0900

    02-Comparison-Logical-Operators

---
+++ 03-Control-Structures/02-Comparison-Logical-Operators/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<h2>Comparison Operators</h2>
+<pre>
+    equal ==
+    identical ===
+    compare > < >= <= <>
+    not equal !=
+    not identical !==
+</pre>
+
+<?php
+
+if (4 !== "4") {
+    echo 'it is true';
+}
+
+?>
+
+</body>
+</html>
