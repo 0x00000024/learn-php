9d97d7c70803fbc39581a5f09177c635035000d8
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 18:12:09 +0900

    05-For-Loop

---
+++ 03-Control-Structures/05-For-Loop/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<h2>Comparison Operators</h2>
+
+<?php
+
+for ($counter = 0; $counter < 10;  $counter++){
+    echo $counter . "<br>";
+}
+
+?>
+
+</body>
+</html>
