2f2cd3c06ff7fdb8a86d7540698e2e51bc102ca5
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 19:28:42 +0900

    update

--- 04-Custom-Functions/03-Return-Values-From-Functions/index.php
+++ 04-Custom-Functions/03-Return-Values-From-Functions/index.php
@@ -8,14 +8,18 @@
 
 <?php
 
-function addNumbers($number1, $number2) {
-    $sum = $number1 + $number2;
-    return $sum;
+function greeting($message) {
+    echo $message . "<br>";
 }
 
-$result = addNumbers(1, 2);
+greeting("hi");
 
-echo $result . "<br>";
+function addNumbers($number1, $number2) {
+    $sum = $number1 + $number2;
+    echo $sum;
+}
+
+addNumbers(1, 2);
 
 ?>
 
