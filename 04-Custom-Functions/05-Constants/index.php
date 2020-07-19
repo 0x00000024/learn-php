5b0f38324d5f26414a639111b0ad590891b8d94c
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 19:31:42 +0900

    05-Constants

---
+++ 04-Custom-Functions/05-Constants/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+$number = 10;
+$number = 1000;
+$number = "1";
+echo $number . "<br>";
+
+define("NAME", 1000);
+
+echo NAME;
+
+?>
+
+</body>
+</html>
