b8a08ec6ebe7ddde2bf40fb7cd7cef2e2eb05dc6
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 14:36:44 +0900

    03-Arrays

---
+++ 02-Data-Types/03-Arrays/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+$numberList = array(24, 432, 546, '5432', '<h1>Hi</h1>');
+
+echo $numberList[0];
+
+echo "<br>";
+
+print_r($numberList);
+
+?>
+
+</body>
+</html>
