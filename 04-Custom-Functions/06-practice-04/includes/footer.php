e40519b7ce05f349839c429d28380177759236da
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 19:45:20 +0900

    update

--- 04-Custom-Functions/06-practice-04/includes/footer.php
+++
@@ -1,7 +0,0 @@
-</section><!--CONTENT-->
-</div><!--Container-->
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-<script src="js/myscript.js"></script>
-</body>
-</html>
