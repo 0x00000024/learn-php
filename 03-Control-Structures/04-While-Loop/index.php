8210e28ad453a2d26210c0ab51012154dfbd14cc
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 18:08:57 +0900

    04-While-Loop

---
+++ 03-Control-Structures/04-While-Loop/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<h2>Comparison Operators</h2>
+
+<?php
+
+$counter = 0;
+
+while ($counter <= 10) {
+    echo $counter;
+    $counter++;
+}
+
+?>
+
+</body>
+</html>
