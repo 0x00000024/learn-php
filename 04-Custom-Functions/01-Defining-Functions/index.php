812ec5c04c8bd417d8b0ec087e419d6cb440ce8b
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 18:34:00 +0900

    01-Defining-Functions

---
+++ 04-Custom-Functions/01-Defining-Functions/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+function say_Something() {
+    echo "hi";
+}
+
+say_Something();
+
+?>
+
+</body>
+</html>
