c4027b98e847d768065f5316e541189319bbaa43
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 18:15:38 +0900

    06-Foreach-Loop

---
+++ 03-Control-Structures/06-Foreach-Loop/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+$numbers = array(12, 34, 56, 78, 91);
+
+foreach ($numbers as $number) {
+    echo $number . "<br>";
+}
+
+?>
+
+</body>
+</html>
