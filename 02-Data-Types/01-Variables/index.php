e7053edb1f3b63845da287b6e84679786fb97803
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 14:01:25 +0900

    01-Variables

---
+++ 02-Data-Types/01-Variables/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+$name = 'Ethan';
+$nunmber = 100;
+
+echo $name . " " . $nunmber;
+
+$hello = "<h1>Hello</h1>";
+
+echo $hello;
+
+?>
+
+
+
+</body>
+</html>
