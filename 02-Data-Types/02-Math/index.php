6f3234ce16ccae557a34d538979eaafe9529a6d2
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 14:06:48 +0900

    02-Math

---
+++ 02-Data-Types/02-Math/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>example html</title>
+</head>
+<body>
+
+<?php
+
+echo 24 + 42;
+echo "<br>";
+echo 46 - 97;
+echo "<br>";
+echo 46 * 97;
+echo "<br>";
+echo 46 / 97;
+echo "<br>";
+echo "<br>";
+
+$number1 = 12;
+$number2 = 24;
+
+echo $number1 + $number2;
+
+?>
+
+
+
+</body>
+</html>
