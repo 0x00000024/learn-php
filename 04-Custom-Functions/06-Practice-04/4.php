d483f7bdcff5954b1cc163cf2d38ead7206e6dc0
Ethan Yu <sohbmpethan@gmail.com>
2020-07-19 19:53:36 +0900

    06-Practice-04

---
+++ 04-Custom-Functions/06-Practice-04/4.php
@@ -0,0 +1,39 @@
+<?php include "functions.php"; ?>
+<?php include "includes/header.php"; ?>
+
+    <section class="content">
+
+    <aside class="col-xs-4">
+
+        <?php Navigation(); ?>
+
+
+    </aside><!--SIDEBAR-->
+
+
+    <article class="main-content col-xs-8">
+
+
+        <?php
+
+        /*
+            Step1: Define a function and make it return a calculation of 2 numbers
+
+            Step 2: Make a function that passes parameters and call it using parameter values
+        */
+
+        function multipleNumbers($number1, $number2){
+            return $number1 * $number2;
+        }
+
+        $result = multipleNumbers(2, 4);
+
+        echo $result . "<br>";
+
+        ?>
+
+
+    </article><!--MAIN CONTENT-->
+
+
+<?php include "includes/footer.php"; ?>
